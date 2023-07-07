d3ed3de5213d5744baef9ade120e9eaecd070cc0
alessandroxljesus <138902268+alessandroxljesus@users.noreply.github.com>
2023-07-07 13:52:43 -0300

    Add files via upload

---
+++ atividades1.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class atividades1 {
+    public static void main(String[] args) {
+
+        String nome;
+        int num,num2;
+
+        System.out.println("Digite seu Nome: ");
+        Scanner ler = new Scanner(System.in);
+        nome = ler.nextLine();
+
+        System.out.println("Digite um Número para Somar com 2: ");
+        num = ler.nextInt();
+
+        //ex 1
+        System.out.println("1. Seu nome é: " + nome +"\n");
+
+        //ex 2
+        num2 = num + 2;
+        System.out.println("2. O Resultado de " + num + " somado com 2, é igual a: " + num2 +"\n");
+
+        //ex 3
+        System.out.println("3. **********\n");
+
+        //ex 4
+        System.out.println("4.");
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*****\n");
+
+
+        //ex 5
+        System.out.println("5.");
+        System.out.println("      #       ");
+        System.out.println("     # #      ");
+        System.out.println("    #   #     ");
+        System.out.println("   #     #    ");
+        System.out.println("  #       #   ");
+        System.out.println(" ###########  \n");
+
+        //ex 6
+        System.out.println("6.");
+        System.out.println("    .     ");
+        System.out.println("   . .    ");
+        System.out.println("  .   .   ");
+        System.out.println(" .     .  ");
+        System.out.println("  .   .   ");
+        System.out.println("   . .    ");
+        System.out.println("    .     \n");
+
+        //ex 7
+        System.out.println("7.");
+        System.out.println("       $      ");
+        System.out.println("      $ $     ");
+        System.out.println("     $   $    ");
+        System.out.println("    $     $   ");
+        System.out.println("   $$$$$$$$$  ");
+
+        //ex 8
+        System.out.println("8.");
+        System.out.println("       *       ");
+        System.out.println("      * *      ");
+        System.out.println("     *   *     ");
+        System.out.println("    *     *    ");
+        System.out.println("   *       *   ");
+        System.out.println("  *         *  ");
+        System.out.println(" *           * ");
+        System.out.println("******   ******");
+        System.out.println("     *   *     ");
+        System.out.println("     *****     \n");
+
+        //ex 9
+        System.out.println("9.");
+        System.out.println("  ***   ***  ");
+        System.out.println("*     *     *");
+        System.out.println(" *         * ");
+        System.out.println("   *     *   ");
+        System.out.println("     * *     \n");
+
+        //ex 10
+        System.out.println("10.");
+        System.out.println("    .....    ");
+        System.out.println("  .       .  ");
+        System.out.println(" .         . ");
+        System.out.println(" .         . ");
+        System.out.println("  .       .  ");
+        System.out.println("    .....    \n");
+
+        //ex 11
+        System.out.println("11.");
+        System.out.println("        >>>>    ");
+        System.out.println(">>>>>>>>>>   >  ");
+        System.out.println(">              >");
+        System.out.println(">>>>>>>>>>   >  ");
+        System.out.println("        >>>>    \n");
+
+        //ex 12
+        System.out.println("12.");
+        System.out.println("    <<<<          ");
+        System.out.println("  <    <<<<<<<<<< ");
+        System.out.println("<               < ");
+        System.out.println("  <    <<<<<<<<<< ");
+        System.out.println("    <<<<          \n");
+
+        //ex 13
+        System.out.println("13. ");
+        System.out.println("      *          ");
+        System.out.println("     * *         ");
+        System.out.println("*****   *****    ");
+        System.out.println(" *         *     ");
+        System.out.println("   *     *       ");
+        System.out.println("  *   *   *      ");
+        System.out.println("  * *   * *     \n");
+
+        //ex 14
+        System.out.println("14.");
+        System.out.println(" ________");
+        System.out.println("|# # # # |");
+        System.out.println("| # # # #|");
+        System.out.println("|# # # # |");
+        System.out.println("| # # # #|");
+        System.out.println(" -------- ");
+
+        //ex 15.
+        System.out.println("15");
+        System.out.println(" ************* ");
+        System.out.println("  *         *  ");
+        System.out.println("   *       *   ");
+        System.out.println("    *     *    ");
+        System.out.println("     *   *     ");
+        System.out.println("      * *      ");
+        System.out.println("       *       ");
+
+    }
+}
